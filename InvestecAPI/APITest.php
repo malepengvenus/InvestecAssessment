7d25c5b8d4130b0fb6451f1c905865e2544397d2
malepengvenus <47916295+malepengvenus@users.noreply.github.com>
2022-02-17 13:35:52 +0200

    Add files via upload

---
+++ InvestecAPI/APITest.php
@@ -0,0 +1,52 @@
+<?php
+
+/* 
+
+ */
+
+$url = "https://swapi.dev/api/people/3";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "Accept: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+$httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+curl_close($curl);
+
+$parsed_resp = json_decode($resp);
+echo 'HTTP Response Code: '.$httpcode.'<br>';
+//echo $parsed_resp;
+//if statement to check if Response status is ok
+if($httpcode == 200){
+    echo 'HTTP Response Status: OK<br>';
+	//for loop to pass the value for skin color
+foreach ($parsed_resp as $key => $value) {
+    if($key == 'name' || $key == 'skin_color'){
+        echo 'Returned '.$value.' for '.$key.'<br>';
+    }
+}
+}elseif($httpcode == 400){
+    echo 'HTTP Response Status: Bad Request<br>';
+}elseif($httpcode == 401){
+    echo 'HTTP Response Status: Unauthorized<br>';
+}elseif($httpcode == 403){
+    echo 'HTTP Response Status: Forbidden<br>';
+}elseif($httpcode == 404){
+    echo 'HTTP Response Status: Not Found<br>';
+}elseif($httpcode == 408){
+    echo 'HTTP Response Status: Request Timeout<br>';
+}elseif($httpcode == 500){
+    echo 'HTTP Response Status: Internal Server Error<br>';
+} else {
+    echo 'Other error occurred';
+}
+?>
